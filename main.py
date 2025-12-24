97d466b90e117b1751305b11edc754cce5ee0731
dmasliuk <dmasliuk.w@gmail.com>
2025-12-24 14:41:06 -0700

    added ai requesting by hardcoding request

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent-py!")
+    if api_key == None:
+        raise RuntimeError("api key not found")
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=user_prompt)
+    if response.usage_metadata == None:
+        raise RuntimeError("api request failed")
 
-
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
+    
 if __name__ == "__main__":
     main()
